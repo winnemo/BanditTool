a07f65afdb2d2a89c712ab49401f4372261d5527
winnemo <moritzwinnemoeller@gmail.com>
2025-10-17 16:43:54 +0200

    Anpassung Deploy datei

--- banditTool/vite.config.ts
+++ banditTool/vite.config.ts
@@ -1,10 +1,9 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // @ts-expect-error – Plugin type mismatch zwischen vitest/vite
   plugins: [react()],
   base: '/BanditTool/',
   test: {
